0da6f50937f393004bed494cc0fcb8de899c3e8c
buddo-Erik <ejohnson@buddobot.com>
2025-05-30 09:05:54 -0500

    Update firebase.ts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,13 +1,18 @@
-import admin from 'firebase-admin';
+import { initializeApp, cert, getApps } from 'firebase-admin/app';
+import { getFirestore } from 'firebase-admin/firestore';
 
-const serviceAccount = JSON.parse(
-  Buffer.from(process.env.FIREBASE_ADMIN_KEY!, 'base64').toString()
-);
+let serviceAccount;
 
-if (!admin.apps.length) {
-  admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-  });
+if (process.env.FIREBASE_SERVICE_ACCOUNT_KEY) {
+  serviceAccount = JSON.parse(
+    Buffer.from(process.env.FIREBASE_SERVICE_ACCOUNT_KEY, 'base64').toString('utf-8')
+  );
+} else {
+  throw new Error('FIREBASE_SERVICE_ACCOUNT_KEY is not defined');
 }
 
-export const db = admin.firestore();
+const app = getApps().length === 0
+  ? initializeApp({ credential: cert(serviceAccount) })
+  : getApps()[0];
+
+export const db = getFirestore(app);
